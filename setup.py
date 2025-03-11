9ba976a3fbb767e62e018a1b9e266e46281fb647
scssw <90193529+scssw@users.noreply.github.com>
2025-03-11 10:05:35 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,11 @@ setup(
     package_data={
         'shadowsocks': ['README.rst', 'LICENSE']
     },
-    install_requires=[],
+    install_requires=[
+        'cymysql>=0.9.0',
+        'six>=1.10.0',  # Python 2 和 3 兼容性支持
+        'cryptography>=2.1'  # 加密库
+    ],
     entry_points="""
     [console_scripts]
     sslocal = shadowsocks.local:main
@@ -26,11 +30,14 @@ setup(
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Internet :: Proxy Servers',
